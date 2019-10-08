9ca6b51406db042e604c254c97d934356b512856
Alahamora <titovat@me.com>
2019-10-08 15:54:51 +0500

    создание репозитория

---
+++ accordeon.js
@@ -0,0 +1,65 @@
+function myFunc1() {
+    let mybutton = document.querySelector(".acc2");
+
+    if (mybutton.classList.value == 'acc2 visible') {
+
+        mybutton.classList.remove('visible');
+
+    }
+    else {
+        mybutton.classList.add('visible');
+    }
+
+}
+function myFunc2() {
+    let mybutton = document.querySelector(".acc7");
+
+    if (mybutton.classList.value == 'acc7 visible') {
+
+        mybutton.classList.remove('visible');
+
+    }
+    else {
+        mybutton.classList.add('visible');
+    }
+
+}
+function myFunc3() {
+    let mybutton = document.querySelector(".acc8");
+
+    if (mybutton.classList.value == 'acc8 visible') {
+
+        mybutton.classList.remove('visible');
+
+    }
+    else {
+        mybutton.classList.add('visible');
+    }
+
+}
+function myFunc4() {
+    let mybutton = document.querySelector(".acc9");
+
+    if (mybutton.classList.value == 'acc9 visible') {
+
+        mybutton.classList.remove('visible');
+
+    }
+    else {
+        mybutton.classList.add('visible');
+    }
+
+}
+function myFunc5() {
+    let mybutton = document.querySelector(".acc10");
+
+    if (mybutton.classList.value == 'acc10 visible') {
+
+        mybutton.classList.remove('visible');
+
+    }
+    else {
+        mybutton.classList.add('visible');
+    }
+
+}
